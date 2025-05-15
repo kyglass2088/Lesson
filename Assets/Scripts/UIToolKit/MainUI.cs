c88e7f6e0da90d17673e204fb8ec032335816adc
kyglass2088 <kyglass115@gmail.com>
2025-05-15 21:51:22 +0900

    UI Toolkit
    UI Toolkit, UIDocument Lesson
    make TopButton, MaxSliderButton, Slider

--- Assets/Scripts/UIToolKit/MainUI.cs
+++ Assets/Scripts/UIToolKit/MainUI.cs
@@ -1,6 +1,63 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UIElements;
 
-public class MainUI
+public class MainUI : MonoBehaviour
 {
+    [SerializeField] UIDocument myUI;
+
+    private Button TopButton;
+    private Button SliderButton;
+    private Slider mySlider;
+
+    private void Start()
+    {
+        VisualElement root = myUI.rootVisualElement;
+
+        TopButton = root.Q<Button>("TopButton");
+        SliderButton = root.Q<Button>("MaxSliderButton");
+        mySlider = root.Q<Slider>("Volume");
+
+        mySlider.RegisterValueChangedCallback(v =>
+        {
+            var oldValue = v.previousValue;
+            var newValue = v.newValue;
+            Debug.Log(v.newValue);
+        });
+
+        if (TopButton != null)
+        {
+            TopButton.clicked += OnMyButtonClick;
+        }
+
+        if (SliderButton != null)
+        {
+            SliderButton.clicked += OnMyMaxSliderClick;
+        }
+
+    }
     
+    private void OnMyButtonClick()
+    {
+        Debug.Log("Button clicked!");
+    }
+
+    private void OnMyMaxSliderClick()
+    {
+        mySlider.value = 0;
+        StartCoroutine(AddSliderValue());
+    }
+
+    private IEnumerator AddSliderValue()
+    {
+        while (true)
+        {
+            mySlider.value++;
+            yield return new WaitForSeconds(0.05f);
+            if (mySlider.value == mySlider.highValue)
+                break;
+        }
+    }
+
+
 }
