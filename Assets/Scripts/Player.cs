ed9191f1ffec41b0a39805dc527cad7b6eec786c
kyglass2088 <kyglass115@gmail.com>
2025-05-20 21:56:53 +0900

    Character Model Add
    Character Model Add,
    Character Move

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,20 @@
 using NUnit.Framework.Internal;
 using System;
+using System.Diagnostics.Tracing;
 using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private void Start()
+    {
+        Player.OnPlayerHitSomethingEvent += Player_OnPlayerHitSomethingEvent;
+    }
+
+    private void Player_OnPlayerHitSomethingEvent()
+    {
+
+    }
+}
 
 public class Player : MonoBehaviour
 {
@@ -8,71 +22,52 @@ public class Player : MonoBehaviour
 
     public int Power = 100;
     public float Damage;
-    //public MainUI mainUI;
 
-    EventBinding<TestEvent> testEventBinding;
-    EventBinding<PlayerEvent> playerEventBinding;
+    public float moveSpeed = 5f;
+    private float horizontalInput;
+
+    // 이벤트 퍼블리셔
+    public static event Action OnPlayerHitSomethingEvent;
+    public static event Action<GameObject> OnPlayerHitSomethingEventWithObj;
 
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("Item Collision");
-        Power += 50;
-        if (collision.gameObject.name == "item")
-        {
-            Debug.Log("Item Collision");
-        }
-        if (collision.gameObject.name != "item")
-        {
-            //Power -= 50;
-            Debug.Log("Not Item Collision");
-        }
+        OnPlayerHitSomethingEvent?.Invoke();
 
-        //mainUI.UpdatePowerSlider(Power);
+        OnPlayerHitSomethingEventWithObj?.Invoke(collision.gameObject);
     }
 
-    private void OnEnable()
+    private void OnTriggerEnter(Collider other)
     {
-        testEventBinding = new EventBinding<TestEvent>(HandleTestEvent);
-        EventBus<TestEvent>.Register(testEventBinding);
-
-        playerEventBinding = new EventBinding<PlayerEvent>(HandlePlayerEvent);
-        EventBus<PlayerEvent>.Register(playerEventBinding);
-    }
-
-
-    private void HandlePlayerEvent(PlayerEvent @event)
-    {
-        Debug.Log("HandlePlayerEvent  @event.health " + @event.health);
-        Debug.Log("HandlePlayerEvent  @event.mana " + @event.mana);
-    }
-
-    private void HandleTestEvent(TestEvent @event)
-    {
-        Debug.Log("Test event ");
-    }
-
-    int MyMethodWithParam(int number)
-    {
-        Debug.Log("MyMethodWithParam! " + number);
-
-        return number * number;
-    }
-
-    void MyMethod()
-    {
-        Debug.Log("myAction!");
+        OnPlayerHitSomethingEventWithObj?.Invoke(other.gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.W))
+            transform.position.x += Vector3.forward * moveSpeed;
+
         if (Input.GetKeyDown(KeyCode.A))
+            transform.right += Vector3.forward * moveSpeed;
+
+        if (Input.GetKeyDown(KeyCode.S))
+            transform.forward += -Vector3.forward * moveSpeed;
+
+        if (Input.GetKeyDown(KeyCode.D))
+            transform.right += Vector3.forward * moveSpeed;
+
+        //transform.Translate(Vector3.forward * moveSpeed * horizontalInput * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.F))
         {
-            anim.SetTrigger("Run");
+            anim.SetTrigger("Attack01");
         }
         if (Input.GetKeyDown(KeyCode.B))
         {
-            anim.SetTrigger("Walk");
+            anim.SetTrigger("Attack04");
         }
     }
 }
