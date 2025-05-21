dc91d363b23929c80dc3104756097767c8f55aee
kyglass2088 <kyglass115@gmail.com>
2025-05-21 21:48:32 +0900

    Collision and Trigger Lesson
    Collision and Trigger Lesson

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -19,47 +19,79 @@ public class GameManager : MonoBehaviour
 public class Player : MonoBehaviour
 {
     public Animator anim;
+    private Rigidbody rb;
 
     public int Power = 100;
     public float Damage;
 
     public float moveSpeed = 5f;
-    private float horizontalInput;
+    private float jumpForce = 5.0f;
+    private bool isGrounded = true;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     // 이벤트 퍼블리셔
     public static event Action OnPlayerHitSomethingEvent;
     public static event Action<GameObject> OnPlayerHitSomethingEventWithObj;
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        OnPlayerHitSomethingEvent?.Invoke();
-
-        OnPlayerHitSomethingEventWithObj?.Invoke(collision.gameObject);
-    }
-
     private void OnTriggerEnter(Collider other)
     {
+        OnPlayerHitSomethingEvent?.Invoke();
         OnPlayerHitSomethingEventWithObj?.Invoke(other.gameObject);
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        OnPlayerHitSomethingEvent?.Invoke();
+        OnPlayerHitSomethingEventWithObj?.Invoke(collision.gameObject);
+
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            Debug.Log("Ground");
+            isGrounded = true;
+        }
+        else if (collision.gameObject.CompareTag("Item"))
+        {
+            Debug.Log("Item");
+        }
+        else if (collision.gameObject.CompareTag("Trap"))
+        {
+            Power -= 20;
+            Debug.Log("Trap");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
+     
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+        }
 
-        if (Input.GetKeyDown(KeyCode.W))
-            transform.position.x += Vector3.forward * moveSpeed;
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+        }
 
-        if (Input.GetKeyDown(KeyCode.A))
-            transform.right += Vector3.forward * moveSpeed;
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += Vector3.forward * moveSpeed * Time.deltaTime;
+        }
 
-        if (Input.GetKeyDown(KeyCode.S))
-            transform.forward += -Vector3.forward * moveSpeed;
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position -= Vector3.forward * moveSpeed * Time.deltaTime;
+        }
 
-        if (Input.GetKeyDown(KeyCode.D))
-            transform.right += Vector3.forward * moveSpeed;
-
-        //transform.Translate(Vector3.forward * moveSpeed * horizontalInput * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.Space)&&isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
 
         if (Input.GetKeyDown(KeyCode.F))
         {
